835c952d37957182afb2ba77508ed12fda5fa79c
Frtmnml <joshuacentenohrrr@gmail.com>
2023-12-23 10:08:14 -0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Un muchacho guapo, sabroson, una vez me rompi un huevo en patineta, pero eso le pasa a cualquiera</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
