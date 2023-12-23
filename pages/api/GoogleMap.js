4b99f8ea852bd76ec4abad94c268630b372ea566
Frtmnml <joshuacentenohrrr@gmail.com>
2023-12-23 14:21:43 -0600

    Adding Costa Rica bounders

--- pages/api/GoogleMap.js
+++ pages/api/GoogleMap.js
@@ -6,20 +6,35 @@ const containerStyle = {
   height: '400px',
 };
 
-const center = {
-  lat: -34.397,
-  lng: 150.644,
+const costaRicaCenter = {
+  lat: 9.7489,
+  lng: -83.7534,
 };
+
+const defaultBounds = {
+    north: 11.2194,
+    south: 8.0329,
+    east: -82.5466,
+    west: -85.9508,
+}
+
+const markers = [
+  { id:1, position: {lat: 9.7489, lng: -83.7534}, title: 'San Jose' },
+  { id:2, position: {lat: 10.3157, lng: -84.4398}, title: 'Liberia' },
+]
 
 function Map() {
   return (
     <LoadScript googleMapsApiKey="AIzaSyC-znCKyB5xFUlglN4LWHs8GiHeMpV_ygU">
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={center}
+        center={costaRicaCenter}
         zoom={10}
+        options={{ restriction: { latLngBounds: defaultBounds, strictBounds: false}}}
       >
-        <Marker position={center} />
+        {markers.map((marker) =>(
+          <Marker key={marker.id} position={marker.position} title={marker.title}/>
+        ))}
       </GoogleMap>
     </LoadScript>
   );
